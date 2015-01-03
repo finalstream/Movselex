8f070b5efd3ddff2c4c40a08c56df76aeb2d884d
finalstream <info@finalstream.net>
2015-01-03 22:44:57 +0900

    もれてたものをこみっと。

---
+++ Movselex.Core/Models/Actions/RefreshAction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movselex.Core.Models.Actions
+{
+    internal class RefreshAction : MovselexActionBase
+    {
+        public override void InvokeCore(MovselexClient client)
+        {
+            // フィルタリングロード
+            client.MovselexFiltering.Load();
+        }
+    }
+}
